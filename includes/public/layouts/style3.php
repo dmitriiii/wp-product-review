5777e600ce0b2852b2410d1563f1cd7f901d0275
Delave-las-Kure <t1evgeny93@gmail.com>
2021-09-08 23:10:59 +0300

    created template structure

--- includes/public/layouts/style3.php
+++ includes/public/layouts/style3.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  *  WPPR front page layout.
  *
@@ -16,39 +17,64 @@ $pros = $review_object->get_pros();
 $cons = $review_object->get_cons();
 
 ?>
-<div id="wppr-review-<?php echo $review_object->get_ID(); ?>"
-	 class="wppr-template wppr-template-default <?php echo is_rtl() ? 'rtl' : ''; ?> wppr-review-container <?php echo( empty( $pros ) ? 'wppr-review-no-pros' : '' ); ?> <?php echo( empty( $cons ) ? 'wppr-review-no-cons' : '' ); ?>">
+<div id="wppr-review-<?php echo $review_object->get_ID(); ?>" class="wppr-template wppr-template-default <?php echo is_rtl() ? 'rtl' : ''; ?> wppr-review-container <?php echo (empty($pros) ? 'wppr-review-no-pros' : ''); ?> <?php echo (empty($cons) ? 'wppr-review-no-cons' : ''); ?>">
 	<section id="review-statistics" class="article-section">
-		<div class="review-wrap-up  cwpr_clearfix">
-			<div class="cwpr-review-top cwpr_clearfix">
-				<h2 class="cwp-item"><?php echo esc_html( $review_object->get_name() ); ?></h2>
-				<span class="cwp-item-price cwp-item">
-					<?php echo esc_html( empty( $price_raw ) ? '' : $price_raw ); ?>
-					<em><?php echo get_field( 'k8_acf_vpndet_curr', $review_object->get_ID() )['label']; ?></em>
-				</span>
+		<div class="review-wrap-up">
+			<div class="cwpr-review-top">
+				<div class="cwpr-score-col cwpr-score-container">
+					<div class="cwpr-score-wrapper">
+						<h2 class="cwpr-score-title"><?php echo esc_html($review_object->get_name()); ?></h2>
+						<div class="cwpr-score-value">100</div>
+					</div>
+					<div class="cwpr-score-level">
+						Trust-Level:
+						<br>
+						"Lorem ipsum"
+					</div>
+				</div>
+				<div class="cwpr-score-col">
+					<div class="cwpr-score-review">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis omnis at similique repellendus doloremque reiciendis aliquid delectus eius laborum pariatur minima, dolor facere possimus atque cupiditate eum rem sapiente maxime.
+					</div>
+				</div>
 			</div><!-- end .cwpr-review-top -->
-			<div class="review-wu-content cwpr_clearfix">
+			<div class="review-wu-content">
 				<div class="review-wu-left">
 					<div class="review-wu-left-top">
 						<div class="rev-wu-image">
-							<?php wppr_layout_get_image( $review_object, 'wppr-default-img', 'photo photo-wrapup wppr-product-image' ); ?>
+							<?php wppr_layout_get_image($review_object, 'wppr-default-img', 'photo photo-wrapup wppr-product-image'); ?>
 						</div>
 
-						<?php wppr_layout_get_rating( $review_object, 'donut', 'default', array( 'review-wu-grade' ) ); ?>
+						<?php wppr_layout_get_rating($review_object, 'donut', 'default', array('review-wu-grade')); ?>
 					</div><!-- end .review-wu-left-top -->
 
-					<?php wppr_layout_get_options_ratings( $review_object, 'dashes' ); ?>
+					<?php wppr_layout_get_options_ratings($review_object, 'dashes'); ?>
 
 				</div><!-- end .review-wu-left -->
 
 				<div class="review-wu-right">
-					<?php wppr_layout_get_pros( $review_object, '', 'h2', '' ); ?>
-					<?php wppr_layout_get_cons( $review_object, '', 'h2', '' ); ?>
+					<?php wppr_layout_get_pros($review_object, '', 'h2', ''); ?>
+					<?php wppr_layout_get_cons($review_object, '', 'h2', ''); ?>
 				</div><!-- end .review-wu-right -->
 
 			</div><!-- end .review-wu-content -->
+			<div class="cwp-footer">
+				<div class="cwpr-footer-col">
+					<span class="cwp-item-price cwp-item">
+						<?php echo esc_html(empty($price_raw) ? '' : $price_raw); ?>
+						<em><?php 
+						if (get_field('k8_acf_vpndet_curr', $review_object->get_ID())) {
+							echo get_field('k8_acf_vpndet_curr', $review_object->get_ID())['label'];  
+						}
+						?></em>
+					</span>
+				</div>
+				<div class="cwpr-footer-col">
+					<?php wppr_layout_get_affiliate_buttons($review_object); ?>
+				</div>
+			</div>
 		</div><!-- end .review-wrap-up -->
 	</section>
 
-	<?php wppr_layout_get_affiliate_buttons( $review_object ); ?>
+
 </div>
