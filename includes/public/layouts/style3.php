63e1bbc7b1440a250150c8bc75aaf35e80fed99f
Delave-las-Kure <t1evgeny93@gmail.com>
2021-09-13 23:35:59 +0300

    show score parameters

--- includes/public/layouts/style3.php
+++ includes/public/layouts/style3.php
@@ -15,26 +15,38 @@ $price_raw = $review_object->get_price_raw();
 
 $pros = $review_object->get_pros();
 $cons = $review_object->get_cons();
+$ranges = get_field('trust_level_ranges', 'option') ? get_field('trust_level_ranges', 'option') : [];
+
+$score = get_field('k8_acf_trust_level') ? (int) get_field('k8_acf_trust_level') : 0;
+$range = array_filter($ranges, function ($range) use ($score) {
+	return  $score >= $range['range_start'] && $range['range_end'] >= $score;
+});
+
+$range = !empty($range) ? array_shift(array_values($range)) : (!empty($ranges) ? $ranges[0] : [
+	'color' => '#a5a5a5',
+	'range_name' => 'N/A',
+	'range_description' => ''
+]);
 
 ?>
 <div id="wppr-review-<?php echo $review_object->get_ID(); ?>" class="wppr-template wppr-template-default <?php echo is_rtl() ? 'rtl' : ''; ?> wppr-review-container <?php echo (empty($pros) ? 'wppr-review-no-pros' : ''); ?> <?php echo (empty($cons) ? 'wppr-review-no-cons' : ''); ?>">
 	<section id="review-statistics" class="article-section">
 		<div class="review-wrap-up">
 			<div class="cwpr-review-top">
-				<div class="cwpr-score-col cwpr-score-container">
+				<div class="cwpr-score-col cwpr-score-container" <? if ($range['color']) {?>style="background-color: <?= $range['color'] ?>" <? } ?>>
 					<div class="cwpr-score-wrapper">
 						<h2 class="cwpr-score-title"><?php echo esc_html($review_object->get_name()); ?></h2>
-						<div class="cwpr-score-value">100</div>
+						<div class="cwpr-score-value"><?= $score ?></div>
 					</div>
 					<div class="cwpr-score-level">
-						Trust-Level:
+						<?= get_field('trust_level_title', 'option') ?>
 						<br>
-						"Lorem ipsum"
+						<?= $range['range_name'] ?>
 					</div>
 				</div>
 				<div class="cwpr-score-col">
 					<div class="cwpr-score-review">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perspiciatis omnis at similique repellendus doloremque reiciendis aliquid delectus eius laborum pariatur minima, dolor facere possimus atque cupiditate eum rem sapiente maxime.
+						<?= str_replace("%vpn", esc_html($review_object->get_name()), $range['range_description']) ?>
 					</div>
 				</div>
 			</div><!-- end .cwpr-review-top -->
@@ -60,18 +72,18 @@ $cons = $review_object->get_cons();
 			</div><!-- end .review-wu-content -->
 			<div class="cwp-footer">
 				<div class="cwpr-footer-col cwpr-price-container">
-					<div class="cwpr-price-desc">Lorem ipsum dolor sit amet consectetur, adipisicing elit.</div>
+					<div class="cwpr-price-desc"><?= str_replace("%vpn", esc_html($review_object->get_name()), get_field('trust_level_text_before_price', 'option')) ?></div>
 					<span class="cwp-item-price cwp-item">
 						<?php echo esc_html(empty($price_raw) ? '' : $price_raw); ?>
-						<em><?php 
-						if (get_field('k8_acf_vpndet_curr', $review_object->get_ID())) {
-							echo get_field('k8_acf_vpndet_curr', $review_object->get_ID())['label'];  
-						}
-						?></em>
+						<em><?php
+							if (get_field('k8_acf_vpndet_curr', $review_object->get_ID())) {
+								echo get_field('k8_acf_vpndet_curr', $review_object->get_ID())['label'];
+							}
+							?></em>
 					</span>
 				</div>
 				<div class="cwpr-footer-col">
-					<div class="cwp-affilate-btn-title">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+					<div class="cwp-affilate-btn-title"><?= str_replace("%vpn", esc_html($review_object->get_name()), get_field('trust_level_text_before_link', 'option')) ?></div>
 					<?php wppr_layout_get_affiliate_buttons($review_object); ?>
 				</div>
 			</div>
