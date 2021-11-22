127b45b66094561ed9621a0ba757c2d3eb6d2d70
Delave-las-Kure <t1evgeny93@gmail.com>
2021-11-22 19:37:28 +0300

    hot fix

--- includes/admin/models/class-wppr-review-model.php
+++ includes/admin/models/class-wppr-review-model.php
@@ -1243,7 +1243,6 @@ class WPPR_Review_Model extends WPPR_Model_Abstract {
 	 * @return array The JSON-LD array.
 	 */
 	public function get_json_ld() {
-		var_dump($this->ID);
 		$k8id = $this->ID;
 		$k8meta = get_post_meta( $k8id );
 		$k8links = unserialize( $k8meta['wppr_links'][0] );
