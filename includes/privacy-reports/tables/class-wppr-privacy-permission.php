bad07d3a1a53add5d62b6fde81bca0cf85cbee03
Delave-las-Kure <t1evgeny93@gmail.com>
2022-01-13 21:34:31 +0300

    add handle for permission db

--- includes/privacy-reports/tables/class-wppr-privacy-permission.php
+++ includes/privacy-reports/tables/class-wppr-privacy-permission.php
@@ -18,18 +18,94 @@ class WPPR_Privacy_Permission extends WPPR_Abstract_Table
         $sql = "CREATE TABLE IF NOT EXISTS {$this->table_name} (
 			`id` int(11) NOT NULL AUTO_INCREMENT,
             `name` varchar(128) NOT NULL,
-            PRIMARY KEY (`id`)
+            PRIMARY KEY (`id`),
+            UNIQUE KEY `name` (`name`)
 		)
         $charset_collate;";
 
         dbDelta($sql);
     }
 
-    public function replace($opts)
+    /**
+     * @param string $permission
+     */
+    public function add($permission)
     {
+        return $this->update($permission) || $this->insert($permission);
     }
 
-    public function get($pid)
+    /**
+     * @param string $permission
+     */
+    public function update($permission)
     {
+        global $wpdb;
+
+        $exist_permission = $this->get_by_name($permission);
+
+        if (!$exist_permission) return false;
+
+        if ($wpdb->update(
+            $this->table_name,
+            [
+                'name' => $permission
+            ],
+            [
+                'id' => $exist_permission['id']
+            ]
+        )) return true;
+
+        return false;
+    }
+
+    /**
+     * @param string $permission
+     */
+    public function insert($permission)
+    {
+        global $wpdb;
+
+        $exist_permission = $this->get_by_name($permission);
+
+        if ($exist_permission) return false;
+
+        if ($wpdb->insert(
+            $this->table_name,
+            [
+                'name' => $permission
+            ]
+        )) return true;
+
+        return false;
+    }
+
+    public function get_by_id($id)
+    {
+        global $wpdb;
+
+        $report = $wpdb->get_row(
+            $wpdb->prepare(
+                "SELECT * FROM $this->table_name WHERE id = %d",
+                $id
+            ),
+            ARRAY_A
+        );
+
+        return $report;
+    }
+
+    public function get_by_name($name)
+    {
+        global $wpdb;
+
+        $report = $wpdb->get_row(
+            $wpdb->prepare(
+                "SELECT * FROM $this->table_name WHERE name = %s",
+                $name
+            ),
+            ARRAY_A
+        );
+
+        return $report;
     }
 }
