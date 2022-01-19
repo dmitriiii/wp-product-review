0334910ea248930d4f56f3f6e38fdbcd5d016a0c
Delave-las-Kure <t1evgeny93@gmail.com>
2022-01-19 03:18:49 +0300

    add more abstraction extends

--- includes/privacy-reports/api/class-wppr-privacy-tracker-api.php
+++ includes/privacy-reports/api/class-wppr-privacy-tracker-api.php
@@ -1,18 +1,22 @@
 <?
-class WPPR_Privacy_Tracker_API
+include_once WPPR_PATH . '/includes/privacy-reports/abstracts/abstract-class-wppr-privacy-api.php';
+
+class WPPR_Privacy_Tracker_API extends WPPR_Abstract_Privacy_API
 {
     function __construct(
-        WPPR_Privacy_Tracker &$tracker_db,
-        WPPR_Privacy_Category &$category_db,
-        WPPR_Privacy_Tracker_Category &$tracker_category_db,
+        WPPR_Privacy_Tracker $tracker_db,
+        WPPR_Privacy_Category $category_db,
+        WPPR_Privacy_Tracker_Category $tracker_category_db,
     ) {
         $this->tracker_db = $tracker_db;
         $this->category_db = $category_db;
         $this->tracker_category_db = $tracker_category_db;
     }
 
-    function update($trackers = [])
+    function update($raw_trackers = [])
     {
+        $trackers = is_array($raw_trackers) ? $raw_trackers : [$raw_trackers];
+
         $cats = array_reduce($trackers, function ($carr, $tracker) {
             if (!isset($tracker['categories'])) return $carr;
             return array_unique(
@@ -23,93 +27,39 @@ class WPPR_Privacy_Tracker_API
         $this->update_categories($cats);
         $this->update_trackers($trackers);
         $this->bind_categories($trackers);
+        $this->unbind_dead_categories($trackers);
     }
 
 
     function update_trackers($trackers)
     {
-        foreach ($trackers as $tracker) {
-            $this->tracker_db->add($tracker);
-        }
+        $this->update_table($trackers, $this->tracker_db);
     }
 
     function update_categories($cats)
     {
-        $db_cats_name = array_map(function ($cat) {
-            return $cat['name'];
-        }, $this->category_db->get_all_by_names($cats));
-
-        foreach ($cats as $cat) {
-            if (in_array($cat, $db_cats_name)) continue;
-            $this->category_db->add($cat);
-        }
+        $this->update_light_table($cats, 'name', $this->category_db);
     }
 
     function bind_categories($trackers)
     {
-        foreach ($trackers as $tracker) {
-            $this->bind_category($tracker['id'], $tracker['categories']);
-        }
+        $this->bulk_update_bind_table(
+            $trackers,
+            'categories',
+            'get_all_tracker_binds',
+            $this->tracker_category_db,
+            $this->category_db
+        );
     }
 
-    function bind_category($tracker_id, $cats)
+    function unbind_dead_categories($trackers)
     {
-        $binds = $this->tracker_category_db->get_all_tracker_binds($tracker_id);
-
-        $db_cats = array_filter($this->category_db->get_all_by_names($cats), function ($cat) use ($binds) {
-            $find = array_filter($binds, function ($bind) use ($cat) {
-                return $bind['category_id'] == $cat['id'];
-            });
-            return empty($find);
-        });
-        $db_cats_name = array_map(function ($cat) {
-            return $cat['name'];
-        }, $db_cats);
-
-        foreach ($cats as $cat) {
-            if (!in_array($cat, $db_cats_name)) {
-                $this->update_categories([$cat]);
-                $db_cat = $this->category_db->get_by_name($cat);
-                $category_id = $db_cat['id'];
-            } else {
-                [$db_cat] = array_filter($db_cats, function ($db_cat) use ($cat) {
-                    return $cat == $db_cat['name'];
-                });
-                $category_id = $db_cat['id'];
-            }
-
-            $this->tracker_category_db->insert($tracker_id, $category_id);
-        }
-    }
-
-    function unbind_category($tracker_id, $cats)
-    {
-        $binds = $this->tracker_category_db->get_all_tracker_binds($tracker_id);
-
-        $db_cats = array_filter($this->category_db->get_all_by_names($cats), function ($cat) use ($binds) {
-            $find = array_filter($binds, function ($bind) use ($cat) {
-                return $bind['category_id'] == $cat['id'];
-            });
-            return empty($find);
-        });
-
-        $db_cats_name = array_map(function ($cat) {
-            return $cat['name'];
-        }, $db_cats);
-
-        foreach ($cats as $cat) {
-            if (!in_array($cat, $db_cats_name)) {
-                $this->update_categories([$cat]);
-                $db_cat = $this->category_db->get_by_name($cat);
-                $category_id = $db_cat['id'];
-            } else {
-                [$db_cat] = array_filter($db_cats, function ($db_cat) use ($cat) {
-                    return $cat == $db_cat['name'];
-                });
-                $category_id = $db_cat['id'];
-            }
-
-            $this->tracker_category_db->insert($tracker_id, $category_id);
-        }
+        $this->bulk_garbage_bind_table(
+            $trackers,
+            'categories',
+            'get_all_tracker_binds',
+            $this->tracker_category_db,
+            $this->category_db
+        );
     }
 }
